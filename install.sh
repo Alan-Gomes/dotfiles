1c128db2bad7f39a5a2047bd53c8d85f34e14151
Alan Gomes <contato@alangomes.dev>
2025-06-30 21:31:07 -0300

    Add Karabiner modifications and improve setup script

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/bin/zsh
-_link_file() {
+
+set -euo pipefail
+
+_link_path() {
   local path="$1"
 
+  if [[ ! -d "$HOME/$(/usr/bin/dirname "$path")" ]]; then
+    mkdir -p "$HOME/$(/usr/bin/dirname "$path")"
+  fi
+
   /bin/ln -sf "$PWD/$path" "$HOME/$path"
+  echo "Linked to $HOME/$path"
 }
 
-_link_file .zshrc
-_link_file .aerospace.toml
-_link_file .gitconfig
-_link_file Library/Application\ Support/aichat/config.yaml
-_link_file Library/Application\ Support/Code/User/keybindings.json
+
+_link_path .zshrc
+_link_path .gitconfig
+
+# macOS only
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  _link_path .aerospace.toml
+  _link_path .config/karabiner/assets/complex_modifications/globe_to_f18.json
+  _link_path .config/karabiner/assets/complex_modifications/aerospace.json
+  _link_path Library/Application\ Support/aichat/config.yaml
+  _link_path Library/Application\ Support/Code/User/keybindings.json
+
+  aerospace reload-config
+fi
