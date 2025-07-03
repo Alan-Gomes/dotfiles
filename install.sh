18c0882ce1d6ce461d890525e123bf8846e88f68
Alan Gomes <contato@alangomes.dev>
2025-07-03 11:21:37 -0300

    Map mouse buttons to aerospace commands

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ _link_path .gitconfig
 if [[ "$(uname -s)" == "Darwin" ]]; then
   _link_path .aerospace.toml
   _link_path .config/karabiner/assets/complex_modifications/globe_to_f18.json
+  _link_path .config/karabiner/assets/complex_modifications/mouse_buttons.json
   _link_path .config/karabiner/assets/complex_modifications/aerospace.json
   _link_path Library/Application\ Support/aichat/config.yaml
   _link_path Library/Application\ Support/Code/User/keybindings.json
